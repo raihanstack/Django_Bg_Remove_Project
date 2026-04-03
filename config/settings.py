6c1c50d0e396d11231050a6febd7a0a93b692dfe
Mostafa Raihan <raihan.invite@gmail.com>
2026-04-03 09:20:39 +0600

    debug false

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5@d3mimklh9+jyg6zj9_37doa^c7ec-tisvi6+y1!o8c3rb2@9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'rsbgeraser.vercel.app',
